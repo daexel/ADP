b2d16b1a7db95051f39861f53f755a7d0343ecb2
me <philipp.beck@haw-hamburg.de>
2017-04-17 11:47:30 +0200

    Added: extending Counter-Class, visulization with main

--- ADP/src/adp04/PascalRekursiv.java
+++ ADP/src/adp04/PascalRekursiv.java
@@ -3,50 +3,59 @@
  */
 package adp04;
 
+import Counter.Counter;
+
 /**
  * @author Michel Gerlach
  * 
- * Rekursive Berechnung des Pascalschen Dreiecks
+ *         Rekursive Berechnung des Pascalschen Dreiecks
  *
  */
-public class PascalRekursiv implements IPascalDreieck {
-	
-	private int aufwandsZaehler=0;
-	
-	public int getAufwand(){
-		return this.aufwandsZaehler;
-	}
-	
+public class PascalRekursiv extends Counter implements IPascalDreieck {
+
+	// private int aufwandsZaehler=0; // abeglöst durch Counter klasse
+
+	// public int getAufwand(){
+	// return this.aufwandsZaehler;
+	// }
+
 	/**
 	 * Funktion für die Berechnung des Pascalschen Dreiecks
 	 * 
-	 * @param zeile des Dreiecks
-	 * @param spalte des Dreiecks
+	 * @param zeile
+	 *            des Dreiecks
+	 * @param spalte
+	 *            des Dreiecks
 	 * 
 	 * @return Wert der jeweiligen Zeile und Spalte
 	 */
 	public int function_pascal(int zeile, int spalte) {
-		if (spalte==0 || spalte==zeile){
-			aufwandsZaehler++;
+		if (spalte == 0 || spalte == zeile) {
+			count();
 			return 1;
 		}
-		aufwandsZaehler++;
-		return function_pascal(zeile-1,spalte) + function_pascal(zeile-1,spalte-1);
-		}
+		count();
+		return function_pascal(zeile - 1, spalte) + function_pascal(zeile - 1, spalte - 1);
+	}
+
 	/**
 	 * Ausgabe der N-ten Zeile des Pascalschen Dreiecks
 	 * 
 	 * @param zeile
 	 */
-	public void berechneZeile(int zeile){
-		for(int spalte=0; spalte<=zeile; spalte++){
-			aufwandsZaehler++;
+	public void berechneZeile(int zeile) {
+		for (int spalte = 0; spalte <= zeile; spalte++) {
+			count();
 			System.out.print(function_pascal(zeile, spalte) + " ");
 		}
 	}
-		public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		PascalRekursiv berechnungRekursiv = new PascalRekursiv();
-		berechnungRekursiv.berechneZeile(22);
-		System.out.println("\nAufwand: "+berechnungRekursiv.getAufwand());
+		for (int i = 0; i < 10; i++) {
+			berechnungRekursiv.berechneZeile(i);
+			System.out.println();
 		}
+		System.out.println("\nAufwand: " + berechnungRekursiv.getOpCount());
+	}
 }
