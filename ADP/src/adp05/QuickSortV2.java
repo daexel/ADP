39b5de9b750f4eb7358ed4f32f5267118323f871
daexel <daexel@Laptop-Daexel>
2017-04-25 17:07:18 +0200

    probleme switch case

--- ADP/src/adp05/QuickSortV2.java
+++ ADP/src/adp05/QuickSortV2.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import Counter.Counter;
 
 public class QuickSortV2 extends Counter {
-	public static void main(String[] args) {
-		int laengeArray = 1000;
+		public static void main(String[] args) {
+		int laengeArray = 10;
 		Element[] liste= new Element[laengeArray];
 	    for (int i=0;i<liste.length;i++){
-	    	Element element = new Element(i+1);
-	    	//Element element = new Element((int) (Math.random()*10)); 
+	    	//Element element = new Element(i+1);
+	    	Element element = new Element((int) (Math.random()*laengeArray)); 
 	    	liste[i]=element;
 	    }
 	    for (int i=0;i<10;i++){
@@ -20,32 +20,47 @@ public class QuickSortV2 extends Counter {
 		int low = 0;
 		int high = liste.length - 1;
 		QuickSortV2 sort = new QuickSortV2();
-		sort.quickSort(liste, low, high);
+		sort.quickSort(liste, low, high,"random");
 		for (int i=0;i<10;i++){
 			System.out.print(liste[i].getKey()+" ");
 		    }
 		System.out.println("\nAnzahl Zaehler: "+sort.getOpCount());
 	}
- 
-	public void quickSort(Element[] arr, int low, int high) {
+ /**
+  * Algorithmus zur Sortierung einer Liste
+  * 
+  * @param arr 				Array als uebergebene Liste	
+  * @param low				Niedrigstes Element
+  * @param high				Hoechstes Element
+  * @param pivotElement		String zur Festlegung des Pivotelementes
+  */
+	public void quickSort(Element[] arr, int low, int high,String pivotElement) {
 		if (arr == null || arr.length == 0)
 			return;
  
 		if (low >= high)
 			return;
- 
-		// pick the pivot
-		// Mitte
-		int middle = low + (high - low) / 2;
-		//Erstes
-		int first = low;
-		//Letztes
-		int last = high;
-		//Radom
-		int random = arr[(int)(Math.random()*(arr.length-1))].getKey();
-		//---------------------------------------------------------------------
-		int pivot = arr[low].getKey();
-		//-----------------------------------------------------------------------
+		int pivot=0;
+		
+		// Definiert das Pivotelement
+		switch(pivotElement){
+		case "middle":
+			int middle = low + (high - low) / 2;
+		    pivot = arr[middle].getKey();
+		    break;
+		case "first":
+			int first = low;
+			pivot = arr[first].getKey();
+			break;
+		case "last":
+			int last = high;
+			pivot = arr[last].getKey();
+			break;
+		case "random":
+			int random = arr[(int)(Math.random()*(arr.length-1))].getKey();
+			pivot = arr[random].getKey();
+			break;
+		}
 		// make left < pivot and right > pivot
 		int i = low, j = high;
 		while (i <= j) {
@@ -58,6 +73,7 @@ public class QuickSortV2 extends Counter {
 			}
  
 			if (i <= j) {
+				//swap elements
 				Element temp = arr[i];
 				arr[i] = arr[j];
 				count();
@@ -68,11 +84,11 @@ public class QuickSortV2 extends Counter {
 			}
 		}
  
-		// recursively sort two sub parts
+		// Rekursives sortieren zweier einzelner Stücke des Arrays
 		if (low < j)
-			quickSort(arr, low, j);
+			quickSort(arr, low, j,pivotElement);
  
 		if (high > i)
-			quickSort(arr, i, high);
+			quickSort(arr, i, high,pivotElement);
 	}
 }
--- ADP/src/tests_ADP05/QuicksortTest.java
+++ ADP/src/tests_ADP05/QuicksortTest.java
@@ -17,38 +17,87 @@ import adp05.QuickSortV2;
  *
  */
 public class QuicksortTest {
+	
+	//Erstellen von TestElementen
 	private Element element1 = new Element(1);
 	private Element element2 = new Element(2);
 	private Element element3 = new Element(3);
 	private Element element4 = new Element(4);
 	private Element element5 = new Element(5);
 
-	@Test
-	public void quicksortTest(){
-	
-	
+	//Erstellen von TestArrays
 	Element [] expectedResult={element1, element2, element3, element4, element5};
 	Element [] arrayQuick1={element1,element2,element5,element4,element3};
 	Element [] arrayQuick2={element5, element4, element3, element2, element1};
 	
 	
-	Element [] sortiert1;
+	@Test
+	public void quickSortFirstTest(){
 	
 	QuickSortV2 qs = new QuickSortV2();
-	qs.quickSort(arrayQuick1,0, 4);
+	qs.quickSort(arrayQuick1,0, 4,"first");
 	
 	for (int i=0; i<expectedResult.length;i++){
 		assertTrue(arrayQuick1[i].getKey()== expectedResult[i].getKey());
 	}
 	
 	QuickSortV2 qs2 = new QuickSortV2();
-	qs.quickSort(arrayQuick2,0, 4);
+	qs2.quickSort(arrayQuick2,0, 4,"first");
 	
 	for (int i=0; i<expectedResult.length;i++){
 		assertTrue(arrayQuick2[i].getKey()== expectedResult[i].getKey());
 	}
 	}
 	
-
+	@Test
+	public void quickSortLastTest(){
+		QuickSortV2 qs = new QuickSortV2();
+		qs.quickSort(arrayQuick1,0, 4,"last");
+		
+		for (int i=0; i<expectedResult.length;i++){
+			assertTrue(arrayQuick1[i].getKey()== expectedResult[i].getKey());
+		}
+		
+		QuickSortV2 qs2 = new QuickSortV2();
+		qs2.quickSort(arrayQuick2,0, 4,"last");
+		
+		for (int i=0; i<expectedResult.length;i++){
+			assertTrue(arrayQuick2[i].getKey()== expectedResult[i].getKey());
+		}
+	}
 	
+	@Test
+	public void quickSortMiddleTest(){
+		QuickSortV2 qs = new QuickSortV2();
+		qs.quickSort(arrayQuick1,0, 4,"middle");
+		
+		for (int i=0; i<expectedResult.length;i++){
+			assertTrue(arrayQuick1[i].getKey()== expectedResult[i].getKey());
+		}
+		
+		QuickSortV2 qs2 = new QuickSortV2();
+		qs2.quickSort(arrayQuick2,0, 4,"middle");
+		
+		for (int i=0; i<expectedResult.length;i++){
+			assertTrue(arrayQuick2[i].getKey()== expectedResult[i].getKey());
+		}
+		
+	}
+	
+	@Test
+	public void quickSortRandomTest(){
+		QuickSortV2 qs = new QuickSortV2();
+		qs.quickSort(arrayQuick1,0, 4,"random");
+		
+		for (int i=0; i<expectedResult.length;i++){
+			assertTrue(arrayQuick1[i].getKey()== expectedResult[i].getKey());
+		}
+		
+		QuickSortV2 qs2 = new QuickSortV2();
+		qs2.quickSort(arrayQuick2,0, 4,"random");
+		
+		for (int i=0; i<expectedResult.length;i++){
+			assertTrue(arrayQuick2[i].getKey()== expectedResult[i].getKey());
+		}
+	}
 }
